4a921868b1d25e1fd387ea14289940d496942e02
sspalding <s.spalding18764@gmail.com>
2022-02-03 14:19:32 -0500

    changes to motor and beginning combining parts in one code

--- Microcontroller/motor_test/motor_test.ino
+++ Microcontroller/motor_test/motor_test.ino
@@ -1,23 +1,16 @@
-#define motor_control 2
+#define motor_control D2
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
    Serial.begin(115200);
-   pinMode(D2, OUTPUT);
-   pinMode(D3, OUTPUT);
-   pinMode(D4, OUTPUT);
-   pinMode(D7, OUTPUT); 
-   pinMode(2, OUTPUT);
+   pinMode(motor_control, OUTPUT);
+   
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    int i = D2;
-    Serial.println(i);
-    digitalWrite(i, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(motor_control, HIGH);   
     delay(1000);                       // wait for a second
-    digitalWrite(i, LOW);    // turn the LED off by making the voltage LOW
-    digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(motor_control, LOW);   
     delay(1000);                       // wait for a second
 }
