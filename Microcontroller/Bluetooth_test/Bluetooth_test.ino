9174280126709e110d31454d55f4ad32dc27018c
Adam Steinberg <43798340+AdamSteinberg1@users.noreply.github.com>
2022-02-08 14:01:16 -0500

    Basic Bluetooth
    Put example code from documentation into our project. Should be changed eventually.

---
+++ Microcontroller/Bluetooth_test/Bluetooth_test.ino
@@ -0,0 +1,54 @@
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updates by chegewara
+
+    This code makes a BLE server and then writes an int that increments every 2 seconds
+
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "561512ef-6e3b-421c-a94b-2f05ef867ad8"
+#define CHARACTERISTIC_UUID "3a63906c-fcc4-4847-be57-c9f3615f268c"
+
+int counter = 0;
+BLECharacteristic *pCharacteristic;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("BuzzBuzz");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  counter++;
+  pCharacteristic->setValue(counter);
+  Serial.println(counter);
+  delay(2000);
+}
