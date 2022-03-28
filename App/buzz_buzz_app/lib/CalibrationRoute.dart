6d066ef9a27ff431601bf362ae73b8bc2b6e008d
Adam Steinberg <43798340+AdamSteinberg1@users.noreply.github.com>
2022-03-27 20:45:39 -0400

    Calibrate Done

--- App/buzz_buzz_app/lib/CalibrationRoute.dart
+++ App/buzz_buzz_app/lib/CalibrationRoute.dart
@@ -17,12 +17,152 @@ class CalibrationRoute extends StatelessWidget {
         initialData: bioData.currentlyConnected(),
         builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
           if (snapshot.data ?? false) {
-            return const Text("Connected");
+            return const CalibrationSequence();
           } else {
             return const Disconnected();
           }
         },
       ),
+    );
+  }
+}
+
+
+
+class CalibrationSequence extends StatelessWidget {
+  const CalibrationSequence({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text("This page allows you to calibrate the Buzz Buzz Bracelet"
+                        " to your resting heart rate. When your heart rate is "
+                        "significantly higher than the calibrated resting "
+                        "heart rate, the Buzz Buzz Bracelet will vibrate.",
+                textAlign: TextAlign.justify,
+                style: Theme.of(context).textTheme.bodyMedium
+            ),
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          elevation: 5,
+          color: Theme.of(context).secondaryHeaderColor,
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Before starting:", style: Theme.of(context).textTheme.headline6,),
+                Text("\t1. Sit down\n\t2. Breathe normally\n\t3. Stay still",
+                  style: Theme.of(context).textTheme.subtitle2
+              ),
+            ]
+            ),
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          elevation: 5,
+          color: Theme.of(context).secondaryHeaderColor,
+        ),
+        const Expanded(child: MeasureHR())
+      ],
+    );
+  }
+}
+
+class MeasureHR extends StatefulWidget {
+  const MeasureHR({Key? key}) : super(key: key);
+
+  @override
+  State<MeasureHR> createState() => _MeasureHRState();
+}
+
+class _MeasureHRState extends State<MeasureHR> with TickerProviderStateMixin{
+  static const measureDuration = Duration(seconds: 30);
+  late AnimationController controller;
+  final List<double> _heartrates = [];
+  bool _measuring = false;
+  bool _completed = false;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: measureDuration,
+    )..addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void measure() async {
+    _heartrates.clear();
+    var subscription = bioData.heartRateStream().listen((value) => _heartrates.add(value));
+    controller.reset();
+    controller.animateTo(1.0);
+    setState(() {
+      _measuring = true;
+      _completed = false;
+    });
+
+    await Future.delayed(measureDuration);
+
+    subscription.cancel();
+    double average = _heartrates.reduce((a,b)=>(a+b))/_heartrates.length;
+    options.setRestingHeartrate(average);
+    setState(() {
+      _measuring = false;
+      _completed = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height:200,
+          child: _measuring
+          ? Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: LinearProgressIndicator(value: controller.value),
+              ),
+              const SizedBox(height: 10),
+              const Text("Measuring Heart Rate\nPlease Wait", textAlign: TextAlign.center),
+            ],
+          )
+          : _completed
+          ? Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.check, size: 100, color: Theme.of(context).primaryColor),
+              const Text("Calibration Complete"),
+            ],
+          )
+          : Icon(Icons.favorite, size: 100, color: Theme.of(context).primaryColor),
+        ),
+        ElevatedButton(
+            onPressed: _measuring ? null : measure,
+            child: Text("Calibrate" + (_completed ? " Again" : "")),
+        )
+      ],
     );
   }
 }
@@ -34,6 +174,7 @@ class Disconnected extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Card(
+        color: Theme.of(context).secondaryHeaderColor,
         margin: const EdgeInsets.all(20),
         elevation: 10,
         child: Column(
@@ -62,5 +203,3 @@ class Disconnected extends StatelessWidget {
     );
   }
 }
-
-
--- App/buzz_buzz_app/lib/Options.dart
+++ App/buzz_buzz_app/lib/Options.dart
@@ -1,14 +1,16 @@
+import 'dart:async';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Options {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   int? _buzzMode;
-
+  double? _restingHeartrate;
   Options();
 
   Future<int> getBuzzMode() {
     if (_buzzMode == null) {
-      return _readMode();
+      return _readMode().then((value) => _buzzMode=value);
     } else {
       return Future.value(_buzzMode);
     }
@@ -26,5 +28,28 @@ class Options {
   Future<int> _readMode() async{
     final SharedPreferences prefs = await _prefs;
     return prefs.getInt("buzzMode") ?? 0;
+  }
+
+  Future<double?> getRestingHeartrate() {
+    if (_restingHeartrate == null) {
+      return _readRestingHeartrate().then((value) => _restingHeartrate = value);
+    } else {
+      return Future.value(_restingHeartrate);
+    }
+  }
+
+  Future<double?> _readRestingHeartrate() async {
+    final SharedPreferences prefs = await _prefs;
+    return prefs.getDouble("restingHeartrate");
+  }
+
+  void setRestingHeartrate(double heartrate) {
+    _restingHeartrate = heartrate;
+    _writeRestingHeartrate(heartrate);
+  }
+
+  void _writeRestingHeartrate(double heartrate) async{
+    final SharedPreferences prefs = await _prefs;
+    prefs.setDouble("restingHeartrate", heartrate);
   }
 }
